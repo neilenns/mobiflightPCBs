d7fbd15418e726fc8bc93ed0d3b861f5a8043186
Neil Enns <info@danecreekphotography.com>
2021-07-20 06:59:15 -0700

    Updated mobiflight configurations to use SimConnect events

---
+++ MobiFlight configurations/touch.cmd
@@ -0,0 +1,3 @@
+@echo off
+copy /b "F:\FlightSim\Community\mobiflight-event-module\layout.json"+,,"F:\FlightSim\Community\mobiflight-event-module\layout.json"
+copy /b "F:\FlightSim\Community\mobiflight-event-module\modules\StandaloneModule.wasm"+,,"F:\FlightSim\Community\mobiflight-event-module\modules\StandaloneModule.wasm"
